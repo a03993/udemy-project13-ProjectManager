1966f929e134e6b05e0ce0d40447f195420c97b3
Tina <tina@Tinas-MacBook-Air.local>
2025-01-02 11:48:47 +0800

    Feat: Add functionality to create and add new projects

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    project: [],
+    projects: [],
   });
 
   function handleStartAddProject() {
@@ -19,10 +19,25 @@ function App() {
     });
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      };
+    });
+  }
+
+  console.log(projectsState);
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />;
+    content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
