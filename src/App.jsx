39024384dc7c4d1330e05b9e50a324a972831389
Tina <tina@Tinas-MacBook-Air.local>
2025-01-02 10:44:39 +0800

    Feat: Add NewProject and Input components styled with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./assets/components/NewProject";
 import ProjectsSidebar from "./assets/components/ProjectsSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NewProject />
     </main>
   );
 }
