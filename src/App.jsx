9f1368ffeef59ee60190ae28d56e85288fda5e65
Tina <tina@Tinas-MacBook-Air.local>
2025-01-02 11:12:45 +0800

    Feat: Add NoProjectSelected component and conditionally render it based on selectedProjectedId state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react";
+
 import NewProject from "./assets/components/NewProject";
 import ProjectsSidebar from "./assets/components/ProjectsSidebar";
+import NoProjectSelected from "./assets/components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    project: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
